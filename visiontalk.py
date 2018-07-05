4e247e71a616566065d3bdeab8f7ce5f81645394
Ktrips <kenichi_yoshida@hotmail.com>
2018-07-05 15:39:28 +0900

    Update visiontalk.py

--- visiontalk.py
+++ visiontalk.py
@@ -17,13 +17,16 @@ import aiy.audio
 import aiy.voicehat
 
 #aiy.i18n.set_language_code('ja-JP')
+default_speech = 'ja-JP'
+
+dir_image = '/home/pi/AIY-projects-python/src/robot/image/'
 
 DISCOVERY_URL="https://{api}.googleapis.com/$discovery/rest?version={apiVersion}"
 
 def camera():
     now = datetime.now()
     dir_name = now.strftime('%Y%m%d')
-    dir_path = '/home/pi/AIY-projects-python/src/robot/image/' + dir_name + '/'
+    dir_path = dir_image + dir_name + '/'
     file_name= now.strftime('%H%M%S') + '.jpg'
     fname    = dir_path + file_name
     try:
@@ -46,7 +49,7 @@ def main(detection, photo_file):
     with open(photo_file, 'rb') as image:
         image_content = base64.b64encode(image.read())
         if detection == "":
-          DETECT = ["FACE", "LABEL", "LOGO"]
+          DETECT = ["FACE", "LABEL", "LOGO", "WORD"]
         else:
           DETECT = [detection.upper()]
 
@@ -72,6 +75,9 @@ def main(detection, photo_file):
               if DET in ["LABEL", "LOGO"]:
                 if res["score"] > 0.7:
                   result += res["description"]+","
+                
+              elif DET in ["WORD"]:
+                if 
 
               elif DET in ["FACE"]:
                 if res["joyLikelihood"] == "VERY_LIKELY":
@@ -97,7 +103,8 @@ if __name__ == '__main__':
      parser = argparse.ArgumentParser()
      parser.add_argument('detection', nargs='?', default='', help='LABEL, FACE, LOGO_DETECTION')
      parser.add_argument('image', nargs='?', default='', help='Image file name')
+     parser.add_argument('speech', nargs='?', default='ja-JP', help='')
      args = parser.parse_args()
      result = main(args.detection, args.image)
      print(result)
-     aiy.audio.say(result, "en-US")
+     aiy.audio.say(result, args.speech)
