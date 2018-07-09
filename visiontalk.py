1d83895cdfd74ca0fb5d218a461f6848422c0761
Ktrips <kenichi_yoshida@hotmail.com>
2018-07-09 10:20:04 +0900

    Update visiontalk.py

--- visiontalk.py
+++ visiontalk.py
@@ -16,16 +16,32 @@ from PIL import Image, ImageDraw
 import aiy.audio
 import aiy.voicehat
 
+from pixels import pixels
 #aiy.i18n.set_language_code('ja-JP')
-aiy_lang = []
-default_speech = 'ja-JP'
-default_detect = ["FACE", "LABEL", "LOGO"]
-default_max    = 3
+#default_speech= 'en-US'
+default_trans = 'ja-JP'
+aiy_lang = ['en-US', 'en-GB', 'de-DE', 'es-ES', 'fr-FR', 'it-IT']
 
-dir_image = '/home/pi/AIY-projects-python/src/smart/image/'
+import urllib.request, urllib.parse, urllib.error
+DEVICE = 0
+CARD   = 0
+VOLUME = 50
+aquest_dir = '/home/pi/AIY-projects-python/src/aquestalkpi/AquesTalkPi'
 
-DISCOVERY_URL="https://{api}.googleapis.com/$discovery/rest?version={apiVersion}"
-
+from google.cloud import translate
+def translate_text(text, trans_lang):
+    if trans_lang == '':
+        return text
+    else:
+      target_lang = trans_lang.split("-")[0]
+      translate_client = translate.Client()
+      result = translate_client.translate(text, target_language=target_lang)
+      return result['translatedText']
+    
+default_detect= ["FACE", "LABEL", "LOGO"]
+default_max   = 3
+dir_image     = '/home/pi/AIY-projects-python/src/smart/image/'
+DISCOVERY_URL = "https://{api}.googleapis.com/$discovery/rest?version={apiVersion}"
 def camera():
     now = datetime.now()
     dir_name = now.strftime('%Y%m%d')
@@ -53,22 +69,25 @@ def draw_boxes(image, bounds, color):
             bound.vertices[3].x, bound.vertices[3].y], None, color)
     return image
 
-def main(detection, photo_file):
+def main(detect="", photo_file="", trans_lang=""):
+    pixels.wake()
     if photo_file == "":
         photo_file = camera()
+    pixels.off()
+    
     credentials = GoogleCredentials.get_application_default()
     service = discovery.build('vision', 'v1', credentials=credentials,
             discoveryServiceUrl=DISCOVERY_URL)
 
+    pixels.listen()
     with open(photo_file, 'rb') as image:
         image_content = base64.b64encode(image.read())
-        if detection == "": #No parameter
+        if detect == "": #No parameter
           DETECT = default_detect
         else: #Paremater specified
-          DETECT = [detection.upper()]
+          DETECT = [detect.upper()]
 
         result   = ""
-        result_ja= ""
         bounds   = []
         tlocale  = ""
         for DET in DETECT:
@@ -90,44 +109,49 @@ def main(detection, photo_file):
             for res in results:
               if DET in ["LABEL", "LOGO"]:
                 if res["score"] > 0.7:
-                  result += res["description"]+","
+                  result += res["description"]+", "
                 
               elif DET in ["TEXT"]:
                 tlocale = res["locale"]
-                result += res["description"]+","
+                result += res["description"]+", "
                 bounds += res["boundingPoly"]["vertices"]
 
               elif DET in ["FACE"]:
-                if res["joyLikelihood"] == "VERY_LIKELY":
+                if res["joyLikelihood"] == "VERY_LIKELY" or res["joyLikelihood"] == "LIKELY":
                   result += "Smile "
-                  result_ja += "笑っている"
-                if res["angerLikelihood"] == "VERY_LIKELY":
+                if res["angerLikelihood"] == "VERY_LIKELY" or res["angerLikelihood"] == "LIKELY":
                   result += "Angry "
-                  result_ja += "怒っている"
-                if res["headwearLikelihood"] == "VERY_LIKELY":
-                  rsult += "Cap "
-                  result_ja += "帽子かぶってる"
-                result += "Face! "
-                result_ja += "人を見つけました！"
-
+                if res["headwearLikelihood"] == "VERY_LIKELY" or res["headwearLikelihood"] == "LIKELY":
+                  rsult += "Capped "
+                
+              result += DET + ", "            
           except:
-            result += "No "+DET
-            result_ja += "いませんでした！"
-
-        return result
+            result += "No " + DET + ", "
+          pixels.off()
+          print('Result: ' + result)
+            
+          pixels.listen()    
+          if trans_lang:
+            trans_text = translate_text(result, trans_lang)
+            trans_text = trans_text.replace("&#39;","")
+            print('Trans: ' + trans_text)
+            if trans_lang in aiy_lang:
+              aiy.audio.say(trans_text, trans_lang)
+            elif trans_lang == "ja-JP":
+              os.system(aquest_dir + ' -g {} {} | aplay -D plughw:{},{}'.format(VOLUME, trans_text, CARD, DEVICE))
+            else:
+              aiy.audio.say('Nothing to trans!', 'en-US')
+            
+          else: #trans_lang = null then default en-US
+            aiy.audio.say(result, 'en-US')
+            
+          pixels.off()
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--detect', nargs='?', default='', help='LABEL, FACE, LOGO_DETECTION')
+    parser.add_argument('--detect', nargs='?', default='', help='LABEL, FACE, LOGO and TEXT_DETECTION')
     parser.add_argument('--image', nargs='?', default='', help='Image file name')
-    parser.add_argument('--speech', nargs='?', default='ja-JP', help='')
+    parser.add_argument('--trans', nargs='?', default='', help='If null no trans (en-US) or trans lang like ja-JP')
     args = parser.parse_args()
-    result = main(args.detect, args.image)
-    print(result)
-    if speech in aiy_lang:
-        aiy.audio.say(result, args.speech)
-    elif speech == 'ja-JP':
-        AquesTalk
-    else:
-       aiy.audio.say('Nothing to talk!', 'en-US')
-
+    main(args.detect, args.image, args.trans)
+    
