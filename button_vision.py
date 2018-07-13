4e82e06b58b6b5cea3b96cd52d999bbf17dce22e
Ktrips <kenichi_yoshida@hotmail.com>
2018-07-13 17:00:02 +0900

    Create button_vision.py

---
+++ button_vision.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import time
+import RPi.GPIO as GPIO
+import subprocess
+
+BUTTON = 17
+LED    = 16
+hold_time=1.5
+
+fname = (time.strftime("%Y%m%d-%H%M%S"))
+fdir  = '/home/pi/web/image/'
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(BUTTON, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(LED, GPIO.OUT)
+
+actions = ['arecord -t wav -f dat -d 5 rec.wav', #ボタン長押しで録音
+           'sudo raspistill -o '+fdir+fname+'.jpg', #ワンプッシュで写真撮影
+           'aplay rec.wav'] #ダブルプッシュで再生
+
+for i in range(3):
+      GPIO.output(LED, GPIO.HIGH)
+      time.sleep(0.5)
+      GPIO.output(LED, GPIO.LOW)
+      time.sleep(0.5)
+
+GPIO.add_event_detect(BUTTON,GPIO.FALLING)
+
+while True:
+     if GPIO.event_detected(BUTTON):
+      GPIO.remove_event_detect(BUTTON)
+      now = time.time()
+      count = 0
+      GPIO.add_event_detect(BUTTON,GPIO.RISING)
+      while time.time() < now + hold_time:
+        if GPIO.event_detected(BUTTON):
+          count +=1
+          time.sleep(.3) # debounce time
+
+      print count
+      if count == 0:
+        GPIO.output(LED, GPIO.HIGH)
+        time.sleep(hold_time)
+        GPIO.output(LED, GPIO.LOW)
+
+      for i in range(count):
+        time.sleep(0.5)
+        GPIO.output(LED, GPIO.HIGH)
+        time.sleep(0.5)
+        GPIO.output(LED, GPIO.LOW)
+
+      cmd = actions[count]
+      print cmd
+      if cmd:
+        subprocess.call(actions[count], shell=True)
+
+      GPIO.remove_event_detect(BUTTON)
+      GPIO.add_event_detect(BUTTON, GPIO.FALLING)
