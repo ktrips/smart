0717db52268101996c6cacb6c6ac23a0541a87d0
Ktrips <kenichi_yoshida@hotmail.com>
2018-07-21 21:48:10 +0900

    Update button_trans.py

--- button_trans.py
+++ button_trans.py
@@ -13,11 +13,11 @@ GPIO.setup(BUTTON, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 GPIO.add_event_detect(BUTTON,GPIO.FALLING)
 
 def main(speech=”en-US”, trans="ja-JP"):
-actions = ['python3 visiontrans.py --detect text --trans {}'.format(trans), #ボタン長押しで、写真撮影、外国語読み取り、変換
+  actions = ['python3 visiontrans.py --detect text --trans {}'.format(trans), #ボタン長押しで、写真撮影、外国語読み取り、変換
            'python3 cloudspeech_trans.py –speech {} --trans {}'.format(speech, trans), #ワンプッシュで外国語聞き取り、変換
            'python3 cloudspeech_trans.py –speech {} --trans {}'.format(trans, speech)] #ダブルプッシュで発話日本語などを外国語変換
-while True:
-     if GPIO.event_detected(BUTTON):
+  while True:
+    if GPIO.event_detected(BUTTON):
       GPIO.remove_event_detect(BUTTON)
       now = time.time()
       count = 0
@@ -35,7 +35,7 @@ while True:
       GPIO.add_event_detect(BUTTON, GPIO.FALLING)
 
 if __name__ == '__main__':
-   parser = argparse.ArgumentParser()
+    parser = argparse.ArgumentParser()
     parser.add_argument('--speech', nargs='?', default='en-US', help='If null speech is en-US')
     parser.add_argument('--trans', nargs='?', default='ja-JP', help='If null trans to ja-JP')
     args = parser.parse_args()
